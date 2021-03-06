2a61b0a5ac5ff89b8f75df4d862977b5a3994c77
Charlie Stableford <charliestableford2@gmail.com>
2021-03-06 15:16:32 -0800

    square

--- 03/script.js
+++ 03/script.js
@@ -1,1 +1,8 @@
 console.log('in');
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh);
