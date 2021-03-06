66baf47d514a9cce7083a19f4e0fad8fa533abc5
Charlie Stableford <charliestableford2@gmail.com>
2021-03-06 15:28:26 -0800

    camera

--- 03/script.js
+++ 03/script.js
@@ -1,4 +1,4 @@
-console.log('in');
+
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1,1,1)
@@ -6,3 +6,18 @@ const material = new THREE.MeshBasicMaterial({color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 
 scene.add(mesh);
+
+const sizes = {
+    width: 800, 
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(72, sizes.width / sizes.height)
+scene.add(camera)
+
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render()
